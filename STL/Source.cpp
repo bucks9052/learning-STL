d3a97ac3e668a033344356cf949eceb6247c4f0a
Oliver Moore <ommore9@gmail.com>
2024-04-17 10:03:38 -0400

    finished up the requirements of the program

--- STL/Source.cpp
+++ STL/Source.cpp
@@ -5,6 +5,14 @@ CIS 1202
 */
 #include "personalLibv1.4.h"
 char character(char start, int offset);
+class invalidCharacterException : public std::runtime_error {
+public:
+    invalidCharacterException() : std::runtime_error("\nINVALID CHARACTER EXCEPTION") {};
+};
+class invalidRangeException : public std::runtime_error {
+public:
+    invalidRangeException() : std::runtime_error("\nINVALID RANGE EXCEPTION") {};
+};
 int main() {
     try {
         int offset = 0;
@@ -14,13 +22,17 @@ int main() {
         cout << "\nEnter a char: ";
         cin >> start;
         if (!isalpha(start)) {
-            throw invalid_argument(" proper character not inputted...");
+            throw invalidCharacterException();
         }
         char result = character(start, offset);
-        
+        if (!isalpha(result)) {
+            throw invalid_argument(" out of range...");
+        }
         cout << "\nresult: " << result;
+    }catch (invalidCharacterException& e) {
+        cerr << "Invalid arguement: " << e.what();
     }
-    catch (invalid_argument& e) {
+    catch (invalidRangeException& e) {
         cerr << "Invalid arguement: " << e.what();
     }
     endProgram();
